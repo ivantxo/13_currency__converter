7e3783028a90659ec8c130da2095608db652a30a
IvanR <ivan.ramirezf@gmail.com>
2024-12-28 01:15:51 +1000

    Added fetch currency from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>Hello</div>;
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [result, setResult] = useState(0);
+
+  useEffect(
+    function () {
+      async function getCurrency() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+        );
+        const data = await res.json();
+        setResult(data.rates[toCurrency]);
+      }
+
+      if (fromCurrency !== toCurrency && amount !== 0) {
+        getCurrency();
+      }
+    },
+    [amount, fromCurrency, toCurrency]
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="AUD">AUD</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="AUD">AUD</option>
+      </select>
+      <p>{result}</p>
+    </div>
+  );
 }
 
 export default App;
